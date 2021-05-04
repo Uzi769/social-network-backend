a094925d445318df91ef9d9dbc368e22752b3589
Gelnaz <gelnaz.abdreeva@irlix.ru>
2021-05-04 16:43:47 +0400

    post

--- src/main/java/com/irlix/irlixbook/dao/model/PostInput.java
+++ src/main/java/com/irlix/irlixbook/dao/model/PostInput.java
@@ -1,32 +1,10 @@
 package com.irlix.irlixbook.dao.model;
 
-import com.irlix.irlixbook.dao.entity.Comment;
-import com.irlix.irlixbook.dao.entity.Tag;
-import com.irlix.irlixbook.dao.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 @Setter
