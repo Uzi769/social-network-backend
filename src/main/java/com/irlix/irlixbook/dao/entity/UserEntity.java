df0a18dcdf9242d6fc5461483fa9b85a129d02a0
Gelnaz <gelnaz.abdreeva@irlix.ru>
2021-05-04 14:22:53 +0400

    init

---
+++ src/main/java/com/irlix/irlixbook/dao/entity/UserEntity.java
@@ -0,0 +1,89 @@
+package com.irlix.irlixbook.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_entity")
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @ToString.Include
+    @NotNull
+    private UUID id;
+
+    @NotEmpty
+    @Column(name = "password")
+    private String password;
+
+    @NotEmpty
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "phone")
+    @Pattern(regexp = "(^\\+?[78][-\\(]?\\d{3}\\)?-?\\d{3}-?\\d{2}-?\\d{2}$)")
+    private String phone;
+
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "is_delete")
+    private boolean delete;
+
+    @Column(name = "minutes_for_booking")
+    private Integer minutesForBooking;
+
+    @Column(name = "password_active")
+    private boolean passwordActive;
+
+    @Column(name = "fcm_token")
+    private String fcm_token;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.getRoles()
+                .stream()
+                .map(i -> new SimpleGrantedAuthority(i.getName()))
+                .collect(Collectors.toList());
+    }
+}
