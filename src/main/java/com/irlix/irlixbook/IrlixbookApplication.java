fabbedad997529246864e07749818c24dd4888e0
blainewerty <hjcn1990rjptk>
2021-05-04 12:14:00 +0300

    Start

---
+++ src/main/java/com/irlix/irlixbook/IrlixbookApplication.java
@@ -0,0 +1,13 @@
+package com.irlix.irlixbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrlixbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrlixbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irlix/irlixbook/IrlixbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irlix.irlixbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrlixbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
