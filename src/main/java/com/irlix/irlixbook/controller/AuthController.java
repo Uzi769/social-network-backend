695dc59bd2b0f5a6cdf47b4de27b3566ca1f52cc
Gelnaz <gelnaz.abdreeva@irlix.ru>
2021-05-05 18:48:29 +0400

    with Auth

--- src/main/java/com/irlix/irlixbook/controller/AuthController.java
+++ src/main/java/com/irlix/irlixbook/controller/AuthController.java
@@ -33,9 +33,8 @@ public class AuthController {
     @CrossOrigin
     @PostMapping("/sign-in")
     public ResponseEntity<String> auth(@RequestBody AuthRequest request) {
-        String token = userService.generateToken(request);
         return ResponseEntity.ok()
-                .header("Authorization", token)
-                .body(token);
+                .header("Authorization", userService.generateToken(request))
+                .body("Auth");
     }
 }
