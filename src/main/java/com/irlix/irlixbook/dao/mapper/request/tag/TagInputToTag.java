3db1c55c057c8c9c3ef0a1d2c259363f64c95b25
Gelnaz <gelnaz.abdreeva@irlix.ru>
2021-05-06 13:39:40 +0400

    correct create tags

--- src/main/java/com/irlix/irlixbook/dao/mapper/request/tag/TagInputToTag.java
+++ src/main/java/com/irlix/irlixbook/dao/mapper/request/tag/TagInputToTag.java
@@ -4,12 +4,14 @@ import com.irlix.irlixbook.dao.entity.Tag;
 import com.irlix.irlixbook.dao.model.tag.TagInput;
 import org.springframework.core.convert.converter.Converter;
 
+import java.util.Locale;
+
 public class TagInputToTag implements Converter<TagInput, Tag> {
 
     @Override
     public Tag convert(TagInput tagInput) {
         return Tag.builder()
-                .name(tagInput.getName())
+                .name(tagInput.getName().toLowerCase(Locale.ROOT))
                 .build();
     }
 }
