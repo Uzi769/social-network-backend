71b294387d2c9405a04c8a18647d3964ce8efebf
aleksey <a.saburov737@gmail.com>
2021-07-09 17:25:16 +0300

    fix config

--- src/main/java/com/irlix/irlixbook/config/ApplicationConfig.java
+++ src/main/java/com/irlix/irlixbook/config/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package com.irlix.irlixbook.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,10 +11,5 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
     }
 }
