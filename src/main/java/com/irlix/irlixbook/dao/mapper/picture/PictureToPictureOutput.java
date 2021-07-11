5c28ce5a44b83b378cde91e7ab96ca5dc95a6316
Gelnaz Abdreeva <gelnaz.abdreeva@irlix.ru>
2021-07-11 19:03:17 +0400

    Исправлены ошибки вывода

--- src/main/java/com/irlix/irlixbook/dao/mapper/picture/PictureToPictureOutput.java
+++ src/main/java/com/irlix/irlixbook/dao/mapper/picture/PictureToPictureOutput.java
@@ -4,6 +4,8 @@ import com.irlix.irlixbook.dao.entity.Picture;
 import com.irlix.irlixbook.dao.model.picture.PictureOutput;
 import org.springframework.core.convert.converter.Converter;
 
+import java.util.stream.Collectors;
+
 public class PictureToPictureOutput implements Converter<Picture, PictureOutput> {
 
     @Override
@@ -11,7 +13,9 @@ public class PictureToPictureOutput implements Converter<Picture, PictureOutput>
         return PictureOutput.builder()
                 .pictureId(picture.getId())
                 .url(picture.getUrl())
-                .contentId(picture.getContent().getId())
+                .contentId(picture.getContent() != null
+                        ? picture.getContent().getId()
+                        : null)
                 .build();
     }
 }
