c05df709bab2777769d09a05c994971a9e8f38d5
aleksey <a.saburov737@gmail.com>
2021-07-14 11:57:48 +0300

    rebuild

--- src/main/java/com/irlix/irlixbook/config/web/WebConfig.java
+++ src/main/java/com/irlix/irlixbook/config/web/WebConfig.java
@@ -31,6 +31,8 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowCredentials(true).maxAge(1800);
+        registry.addMapping("/**").allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(1800);
     }
 }
