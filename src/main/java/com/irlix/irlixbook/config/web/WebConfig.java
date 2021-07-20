3323266675e024d6fabc3ec4101d9a1dace045dd
aleksey <a.saburov737@gmail.com>
2021-07-20 16:58:21 +0300

    cors response headers added

--- src/main/java/com/irlix/irlixbook/config/web/WebConfig.java
+++ src/main/java/com/irlix/irlixbook/config/web/WebConfig.java
@@ -32,6 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .exposedHeaders("*")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
