4d5e03be3f2fc50d5d960b0a8c7561dcdc70ad77
Viacheslav Kurkin <viacheslav.kurkin@irlix.ru>
2021-10-04 22:26:44 +0400

    change chat code

--- src/main/java/com/irlix/irlixbook/dao/mapper/chat/MessageRequestToChatMessage.java
+++ src/main/java/com/irlix/irlixbook/dao/mapper/chat/MessageRequestToChatMessage.java
@@ -5,13 +5,13 @@ import com.irlix.irlixbook.dao.model.chat.request.MessageRequest;
 import org.springframework.core.convert.converter.Converter;
 
 public class MessageRequestToChatMessage implements Converter<MessageRequest, ChatMessage> {
+
     @Override
     public ChatMessage convert(MessageRequest messageRequest) {
         return ChatMessage.builder()
                 .localId(messageRequest.getLocalId())
                 .timestamp(messageRequest.getTimeStamp())
                 .content(messageRequest.getContent())
-                .sender(messageRequest.getUserId())
                 .build();
     }
 }
