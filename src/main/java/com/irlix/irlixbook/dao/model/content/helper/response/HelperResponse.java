8b3d7f07c10f81deed911e8b73883d3dd007a043
Viacheslav Kurkin <viacheslav.kurkin@irlix.ru>
2021-10-25 13:37:00 +0400

    change helper creatingDate format

--- src/main/java/com/irlix/irlixbook/dao/model/content/helper/response/HelperResponse.java
+++ src/main/java/com/irlix/irlixbook/dao/model/content/helper/response/HelperResponse.java
@@ -1,6 +1,11 @@
 package com.irlix.irlixbook.dao.model.content.helper.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.irlix.irlixbook.dao.entity.enams.HelperEnum;
+import com.irlix.irlixbook.utils.serdes.CustomLocalDateTimeDeserializer;
+import com.irlix.irlixbook.utils.serdes.CustomLocalDateTimeSerializer;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -20,6 +25,10 @@ public class HelperResponse {
     private String avatar;
     private String description;
     private String deepLink;
+
+    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
+    @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
+    @Schema(type = "string", example = "2018-01-01T15:02:01")
     private LocalDateTime creatingDate;
     private List<Long> comments;
     private int like;
